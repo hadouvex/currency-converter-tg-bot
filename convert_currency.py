e7b121756ce9f0ba3bb0c93fe4eda25ef9154a1b
hadouvex <undercover.agents.spy@gmail.com>
2020-09-30 18:00:28 +0300

    added cache support, refactored flask_app.py

--- convert_currency.py
+++ convert_currency.py
@@ -1,13 +1,39 @@
 import requests
-
+from datetime import datetime
+from envparse import env
 
 API_URL = 'https://api.exchangeratesapi.io/latest'
 
+USE_CACHE = None
+
+USE_CACHE = bool(int(env('CONVERT_CURRENCY_USE_CACHE', False)))
+
+CURRENCIES_LIST_CACHED = []
+ALL_CURRENCIES_EXCHANGE_RATES_CACHED = {}
+
+CACHE_LAST_UPDATE_TIME = datetime(1, 1, 1, 0, 0)
+
+def cache_currencies_list():
+    global CURRENCIES_LIST_CACHED
+    CURRENCIES_LIST_CACHED = get_currencies_list()
+
+def get_exchange_rates_for_every_currency():
+    data = {}
+    for currency in CURRENCIES_LIST_CACHED:
+        currency_rates = get_exchange_rates(currency)
+        data[currency] = currency_rates
+    return data
+
+def cache_all_currencies_exchange_rates():
+    global ALL_CURRENCIES_EXCHANGE_RATES_CACHED
+    ALL_CURRENCIES_EXCHANGE_RATES_CACHED = get_exchange_rates_for_every_currency()
 
 def get_exchange_rates(currency_name='USD'):
     response = requests.get(API_URL + f'?base={currency_name}')
     return response.json()
 
+def get_exchange_rates_for_chosen_currencies_using_cache(to_convert, convert_to):
+    return ALL_CURRENCIES_EXCHANGE_RATES_CACHED[to_convert]['rates'][convert_to]
 
 def get_exchange_rates_for_chosen_currencies(to_convert, convert_to):
     data = get_exchange_rates(to_convert)
@@ -18,7 +44,37 @@ def convert(to_convert, convert_to, amount):
     rate = get_exchange_rates_for_chosen_currencies(to_convert, convert_to)
     return amount * rate
 
+def check_time_and_update_cache(func):
+    def wrapper(*args, **kwargs):
+        dt_now = datetime.now()
+        if dt_now.hour - CACHE_LAST_UPDATE_TIME.hour >= 1:
+            update_cache()
+            return_value = func(*args, **kwargs)
+            return return_value
+        else:
+            return_value = func(*args, **kwargs)
+            return return_value
+    return wrapper
 
-def list_currencies():
+@check_time_and_update_cache
+def convert_using_cache(to_convert, convert_to, amount):
+    rate = get_exchange_rates_for_chosen_currencies_using_cache(to_convert, convert_to)
+    return amount * rate
+
+def get_currencies_list():
     data = get_exchange_rates()
     return [key for key in data['rates']]
+
+def get_currencies_list_cached():
+    return CURRENCIES_LIST_CACHED
+
+def update_cache():
+    global CACHE_LAST_UPDATE_TIME
+    cache_currencies_list()
+    cache_all_currencies_exchange_rates()
+
+    CACHE_LAST_UPDATE_TIME = datetime.now()
+
+
+if USE_CACHE:
+    update_cache()
--- flask_app.py
+++ flask_app.py
@@ -4,49 +4,76 @@ import convert_currency
 from flask import Flask
 from flask import request
 from flask import jsonify
+
+
+from flask.logging import create_logger
 from bot import send_message
 
 
 app = Flask(__name__)
+logger = create_logger(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        response = request.get_json()
+        process_request(request)
+        return '<h2>Bot is currently working...</h2>'       
+    return '<h2>Bot is currently working...</h2>'
+
+
+def process_request(request):
+    response = request.get_json()
+
+    try:
         chat_id = response['message']['chat']['id']
+    except KeyError:
+        return '<h2>Bot is currently working...</h2>'
+    
+    try:
+        text = response['message']['text']
+    except KeyError:
+        send_message(chat_id, text='Input is incorrect, try using "/help"')
+        return '<h2>Bot is currently working...</h2>'
+    
+    available_currencies_string, available_currencies_list = generate_available_currencies_string_and_list()
 
-        try:
-            text = response['message']['text']
-        except KeyError:
-            send_message(chat_id, text='Input is incorrect, try using "/help"')
-            return '<h2>Bot is currently working...</h2>'
-        
-        available_currencies_list = convert_currency.list_currencies()
-        available_currencies_string = ''
-        for currency in available_currencies_list:
-            available_currencies_string += currency + '\n'
-        if text == '/start':
-            send_message(chat_id, text="Type '/help' to see how-to")
-        elif text == '/help':
-            send_message(chat_id, text='This bot can convert from one currency to another\nType "/convert" to start')
-            send_message(chat_id, text='Available currencies:\n\n' + available_currencies_string)
-        elif text == '/convert':
-            send_message(chat_id, 'Reply using following format:\n"currency1 currency2 amount"')
-        else:
-            text = text.upper()
-            if set(text.split()[0:2]) <= (set(available_currencies_list)):
-                text_splitted = text.split()
+    generate_response(text, chat_id, available_currencies_string, available_currencies_list)
 
+def generate_response(text, chat_id, available_currencies_string, available_currencies_list):
+    if text == '/start':
+        send_message(chat_id, "Type '/help' to see how-to")
+    elif text == '/help':
+        send_message(chat_id, 'This bot can convert from one currency to another\nType "/convert" to start')
+        send_message(chat_id, 'Available currencies:\n\n' + available_currencies_string)
+    elif text == '/convert':
+        send_message(chat_id, 'Reply using following format:\n"currency1 currency2 amount"')
+    else:
+        text = text.upper()
+        if set(text.split()[0:2]) <= (set(available_currencies_list)):
+            text_splitted = text.split()
+            try:
                 to_convert = text_splitted[0]
                 convert_to = text_splitted[1]
                 amount = int(text_splitted[2])
-                    
-                calculated_value = round(convert_currency.convert(to_convert, convert_to, amount), 2)
-                send_message(chat_id, text=f'{amount} {to_convert} = {calculated_value} {convert_to}')
+            except IndexError:
+                send_message(chat_id, text='Input is incorrect, try using "/help"')
+                return '<h2>Bot is currently working...</h2>'
+            
+            if convert_currency.USE_CACHE:
+                calculated_value = round(convert_currency.convert_using_cache(to_convert, convert_to, amount), 2)
             else:
-                send_message(chat_id, text='Something is wrong, try using "/help"')
-                # return jsonify(response)
+                calculated_value = round(convert_currency.convert(to_convert, convert_to, amount), 2)
+            send_message(chat_id, text=f'{amount} {to_convert} = {calculated_value} {convert_to}')
+        else:
+            send_message(chat_id, text='Something is wrong, try using "/help"')
 
-        # return jsonify(response)
-    return '<h2>Bot is currently working...</h2>'
+def generate_available_currencies_string_and_list():
+    if convert_currency.USE_CACHE:
+        available_currencies_list = convert_currency.get_currencies_list_cached()
+    else:
+        available_currencies_list = convert_currency.get_currencies_list()
+    available_currencies_string = ''
+    for currency in available_currencies_list:
+        available_currencies_string += currency + '\n'
+    return available_currencies_string, available_currencies_list
